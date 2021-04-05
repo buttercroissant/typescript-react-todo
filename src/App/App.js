51510b8f8071e65261c05281cc1a1dcfdf688abc
Melvin Tang <melvin@seekasia.com>
2021-04-05 15:00:17 +0800

    initial config

---
+++ src/App/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Todo from "./components/Todo";
+
+const App = () => (
+  <div>
+    <h1>Hello World</h1>
+    <Todo />
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, "public"),
+    hot: true,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+};
